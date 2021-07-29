e328387f71697bb7b1b2f2c35901193989737093
Aprajit Kumar <aprajit.k@gmail.com>
2021-07-29 17:38:27 +0000

    updating letter positions.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,24 +1,24 @@
-// let isEqual = '';
-// const assertArraysEqual = function (array1, array2){
-//   for (i = 0; i < array1.length; i++){
-//     for(j = 0; j < array2.length; j++){
-//       if(array1[i] === array2[j]){
-//         isEqual = `🐕🐕🐕 Assertion Passed: ${array1} === ${array2}`;
-//       }else{
-//         isEqual = `😿😿😿 Assertion Failed:  ${array1} !== ${array2}`;
-//       }
-//     };
-//   };
-//   return isEqual;
-// };
+let isEqual = '';
+const assertArraysEqual = function (array1, array2){
+  for (i = 0; i < array1.length; i++){
+    for(j = 0; j < array2.length; j++){
+      if(array1[i] === array2[j]){
+        isEqual = `🐕🐕🐕 Assertion Passed: ${array1} === ${array2}`;
+      }else{
+        isEqual = `😿😿😿 Assertion Failed:  ${array1} !== ${array2}`;
+      }
+    };
+  };
+  return isEqual;
+};
 
-// const assertEqual = function(actual, expected) {
-//   if (isEqual) {
-//     return `🐕🐕🐕 Assertion Passed: ${actual} === ${expected}`;
-//   } else {
-//     return `😿😿😿 Assertion Failed:  ${actual} !== ${expected}`;
-//   };
-// };
+const assertEqual = function(actual, expected) {
+  if (isEqual) {
+    return `🐕🐕🐕 Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `😿😿😿 Assertion Failed:  ${actual} !== ${expected}`;
+  };
+};
 
 const letterPositions = function(sentence) {
   const finalObject = {};
@@ -37,3 +37,5 @@ const letterPositions = function(sentence) {
 // console.log(letterPositions('Hello'));
 
 console.log(letterPositions('Hello World'));
+
+console.log(assertArraysEqual(letterPositions("hello").e, [1]));
