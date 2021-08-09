b93ce0c7df5fc6dc079321e73672e45ac58ffed0
Aprajit Kumar <aprajit.k@gmail.com>
2021-08-09 18:48:25 +0000

    adding middle and tail tests with chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,20 @@
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
-const words = ["Yo Yo", "Lighthouse" , "Labs"];
+const assert = require("chai").assert;
+// const words = ["Yo Yo", "Lighthouse" , "Labs"];
+
+describe("#tail", () =>{
+  it("returns [Lighthouse, Labs] for [Yo Yo, Lighthouse , Labs]", () =>{
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse" , "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("returns [Milo, Roman] for [Luna, Milo, Roman]", () =>{
+    assert.deepEqual(tail(["Luna", "Milo" , "Roman"]), ["Milo", "Roman"]);
+  });
+});
 
 
-let newArray = tail(words);
-console.log(words);
-console.log(assertEqual(words.length, 3));
-console.log(newArray);
+
+
+// let newArray = tail(words);
+// console.log(words);
+// console.log(assertEqual(words.length, 3));
+// console.log(newArray);
