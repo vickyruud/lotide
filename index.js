f7b2e9b19705a3ebf9d8a127c89f1216b327559b
Aprajit Kumar <aprajit.k@gmail.com>
2021-08-09 19:03:45 +0000

    adding index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+const assertArraysEqual = require("./assertArraysEqual");
+const assertEqual = require("./assertEqual");
+
+
+module.exports = {
+  head: head,
+  middle: middle,
+  tail: tail,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual
+  
+};
