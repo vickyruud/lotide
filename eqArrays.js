0a6167f318bbdfefabb03197f8f115c7ac2e7b25
Aprajit Kumar <aprajit.k@gmail.com>
2021-08-10 13:41:17 +0000

    adding recursive eqarrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,14 @@
-
-let isEqual = ' ';
-const eqArrays = function (array1, array2){
-  for (i = 0; i < array1.length; i++){
-    for(j = 0; j < array2.length; j++){
-      if(array1[i] === array2[j]){
-        isEqual = true;
-      }else{
-        isEqual = false;
-      }
-    };
-  };
+const eqArrays = function(array1, array2) {
+  let isEqual = true;
+  if (array1.length !== array2.length) return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i]) || Array.isArray(array2[i])) {
+      isEqual = isEqual && eqArrays(array1[i], array2[i]);
+    } else if (array1[i] !== array2[i]) {
+      isEqual = isEqual && false;
+    }
+  }
   return isEqual;
 };
 module.exports = eqArrays;
+
